8e95c9008ef239d3ee928970b69a763347e80dc1
DO-III <davidiii@uw.edu>
2022-01-29 14:38:09 -0800

    Clicking only works on last button?

--- GameButton.js
+++ GameButton.js
@@ -4,7 +4,8 @@ const BUTTON_DIMENSION = 150;
 
 class GButton {
 
-    constructor(x, y, color) {
+    constructor(game, x, y, color) {
+        this.game = game;
         this.x = x;
         this.y = y;
         this.color = color;
@@ -14,10 +15,30 @@ class GButton {
     draw(ctx) {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, BUTTON_DIMENSION, BUTTON_DIMENSION);
+
+        if (this.game.click && this.game.mouse) {
+            this.checkClicked(this.game.click.x, this.game.click.y);
+        }
     }
 
     update() {
 
+    }
+
+    /*
+    Make this button flash its assigned color.
+    */ 
+    checkClicked(X, Y) {
+        console.log(X);
+        console.log(Y);
+
+        if(X < this.x + 150 && this.x < X) {
+            if (Y < this.y + 150 && this.y < Y) {
+                this.color = "White";
+            }
+        }
+
+        this.game.click = null;
     }
 
 }
--- main.js
+++ main.js
@@ -8,15 +8,15 @@ ASSET_MANAGER.downloadAll(() => {
 
 
 
-	gameEngine.addEntity(new GButton(0, 100, "Red"));
-	gameEngine.addEntity(new GButton(150, 100, "Blue"));
-	gameEngine.addEntity(new GButton(300, 100, "Green"));
-	gameEngine.addEntity(new GButton(0, 250, "Yellow"));
-	gameEngine.addEntity(new GButton(150, 250, "Black"));
-	gameEngine.addEntity(new GButton(300, 250, "Pink"));
-	gameEngine.addEntity(new GButton(0, 400, "Purple"));
-	gameEngine.addEntity(new GButton(150, 400, "Cyan"));
-	gameEngine.addEntity(new GButton(300, 400, "Brown"));
+	gameEngine.addEntity(new GButton(gameEngine, 0, 100, "Red"));
+	gameEngine.addEntity(new GButton(gameEngine, 150, 100, "Blue"));
+	gameEngine.addEntity(new GButton(gameEngine, 300, 100, "Green"));
+	gameEngine.addEntity(new GButton(gameEngine, 0, 250, "Yellow"));
+	gameEngine.addEntity(new GButton(gameEngine, 150, 250, "Black"));
+	gameEngine.addEntity(new GButton(gameEngine, 300, 250, "Pink"));
+	gameEngine.addEntity(new GButton(gameEngine, 0, 400, "Purple"));
+	gameEngine.addEntity(new GButton(gameEngine, 150, 400, "Cyan"));
+	gameEngine.addEntity(new GButton(gameEngine, 300, 400, "Brown"));
 
 	gameEngine.init(ctx);
 
